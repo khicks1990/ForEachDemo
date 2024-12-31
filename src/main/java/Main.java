c41955a76fc19639337124c102c8eb2c624a61cf
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 06:12:28 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+/**
+   This program demonstrates Iterable forEach().
+*/
+
+public class Main
+{
+    public static void main(String [] args)
+    {
+        // Array of names.
+        String [] names = {"Anna", "Bob", "Carlos", "Debby"};
+
+        // Create list and add names.
+        ArrayList<String> nameList = new ArrayList<>();
+        for (String name : names)
+        {
+            nameList.add(name);
+        }
+
+        // Use forEach with lambda expression to print.
+        nameList.forEach(
+                x -> 
+                 {
+                     System.out.printf("%s %d\n", x, x.length());
+                 });
+    }    
+}
